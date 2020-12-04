feba3e3ad211eb70e759774bc19cc3dcddcf24eb
Siddhant Jha <sonujha573@gmail.com>
2020-12-04 15:04:55 +0530

    Custom version of Elasticmock

---
+++ elasticmock/__init__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from functools import wraps
+
+from elasticsearch.client import _normalize_hosts
+from mock import patch
+
+from elasticmock.fake_elasticsearch import FakeElasticsearch
+
+ELASTIC_INSTANCES = {}
+
+
+def _get_elasticmock(hosts=None, *args, **kwargs):
+    host = _normalize_hosts(hosts)[0]
+    elastic_key = '{0}:{1}'.format(
+        host.get('host', 'localhost'), host.get('port', 9200)
+    )
+
+    if elastic_key in ELASTIC_INSTANCES:
+        connection = ELASTIC_INSTANCES.get(elastic_key)
+    else:
+        connection = FakeElasticsearch()
+        ELASTIC_INSTANCES[elastic_key] = connection
+    return connection
+
+def mock_elasticsearch(path_to_es_inst='elasticseach.Elasticsearch'):
+    def elasticmock(f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            ELASTIC_INSTANCES.clear()
+            with patch(path_to_es_inst, _get_elasticmock()):
+                result = f(*args, **kwargs)
+            return result
+        return decorated
+    return elasticmock
